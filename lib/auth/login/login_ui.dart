8c7309cd9572bc5359eb8594d532d74cd6a1886c
Au Thanakorn <92154391+AuThanakorn@users.noreply.github.com>
2022-09-15 23:42:01 +0700

    refactor authentication

--- lib/auth/login/login_ui.dart
+++ lib/auth/login/login_ui.dart
@@ -10,6 +10,7 @@ import 'package:verbose_share_world/auth/login_navigator.dart';
 import 'package:verbose_share_world/components/custom_button.dart';
 import 'package:verbose_share_world/components/entry_field.dart';
 import 'package:verbose_share_world/generated/l10n.dart';
+import 'package:verbose_share_world/provider/ViewModel/authentication_viewmodel.dart';
 
 import 'package:verbose_share_world/provider/ViewModel/firebase_auth_viewmodel.dart';
 
@@ -19,8 +20,8 @@ class LoginUi extends StatefulWidget {
 }
 
 class _LoginUiState extends State<LoginUi> {
-  bool isLoggingIn = false;
-  TextEditingController _userIDController = TextEditingController(text: "");
+  TextEditingController _userIDController =
+      TextEditingController(text: "johnwick2");
   // TextEditingController _countryController = TextEditingController();
   String? isoCode;
 
@@ -28,144 +29,85 @@ class _LoginUiState extends State<LoginUi> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     var mediaQuery = MediaQuery.of(context);
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: mediaQuery.size.height,
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              // Spacer(),
-              //Image.asset('assets/images/ShareWorldLogo.png', height: 30),
-              Spacer(),
-              Image.asset('assets/Icons/amity-logo-banner.png', height: 100),
-              SizedBox(height: 20),
-              EntryField(
-                controller: _userIDController,
-                hint: 'UserID',
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              EntryField(
-                hint: 'Password',
-              ),
-              SizedBox(height: 40),
-              CustomButton(
-                  label: S.of(context).signIn,
-                  isLoading: isLoggingIn,
-                  onTap: () async {
-                    if (!isLoggingIn) {
-                      setState(() {
-                        isLoggingIn = true;
-                      });
-
-                      await AmitySLEUIKit().registerDevice(
-                        context: context,
-                        userId: _userIDController.text,
-                        callback: (isSuccess, error) {
-                          if (isSuccess) {
-                            print("login Success..");
-                            Navigator.pushNamed(context, LoginRoutes.app);
-                            setState(() {
-                              isLoggingIn = false;
-                            });
-                          } else {
-                            print("error..");
-                            setState(() {
-                              isSuccess = false;
-                            });
-                            AmityDialog().showAlertErrorDialog(
-                                title: "error!", message: error!);
-                          }
+    return Consumer<AuthenTicationVM>(builder: (context, vm, _) {
+      return Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: mediaQuery.size.height,
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                // Spacer(),
+                //Image.asset('assets/images/ShareWorldLogo.png', height: 30),
+                Spacer(),
+                Image.asset('assets/Icons/amity-logo-banner.png', height: 100),
+                SizedBox(height: 20),
+                EntryField(
+                  controller: _userIDController,
+                  hint: 'UserID',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                EntryField(
+                  hint: 'Password',
+                ),
+                SizedBox(height: 40),
+                CustomButton(
+                    label: S.of(context).signIn,
+                    isLoading: vm.isLoading,
+                    onTap: () async {
+                      vm.enterTheAppWith(userId: _userIDController.text);
+                    }),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, LoginRoutes.registration);
                         },
-                      );
-                    }
-                  }),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, LoginRoutes.registration);
-                      },
-                      child: Text("Sign up")),
-                ],
-              ),
-              Spacer(),
-              Text(S.of(context).or_Continue_With,
-                  style: theme.textTheme.headline6!.copyWith(fontSize: 14)),
-              SizedBox(height: 32),
-              // CustomButton(
-              //   icon: Image.asset('assets/Icons/ic_fb.png', scale: 2),
-              //   radius: 12,
-              //   color: Color(0xff3b45c1),
-              //   onTap: () {
-              //     Navigator.pushNamed(context, LoginRoutes.registration);
-              //   },
-              //   label: 'Facebook',
-              //   textColor: Colors.white,
-              // ),
-              // SizedBox(height: 20),
+                        child: Text("Sign up")),
+                  ],
+                ),
+                Spacer(),
+                Text(S.of(context).or_Continue_With,
+                    style: theme.textTheme.headline6!.copyWith(fontSize: 14)),
+                SizedBox(height: 32),
+                // CustomButton(
+                //   icon: Image.asset('assets/Icons/ic_fb.png', scale: 2),
+                //   radius: 12,
+                //   color: Color(0xff3b45c1),
+                //   onTap: () {
+                //     Navigator.pushNamed(context, LoginRoutes.registration);
+                //   },
+                //   label: 'Facebook',
+                //   textColor: Colors.white,
+                // ),
+                // SizedBox(height: 20),
 
-              CustomButton(
-                label: 'Google',
-                radius: 12,
-                borderColor: Colors.black,
-                onTap: () {
-                  if (!isLoggingIn) {
-                    setState(() {
-                      isLoggingIn = true;
-                    });
-                    Provider.of<GoogleSignInProvider>(context, listen: false)
-                        .login((isSuccess, error) async {
-                      if (isSuccess) {
-                        log("tap signIn");
-
-                        await AmitySLEUIKit().registerDevice(
-                          context: context,
-                          userId: Provider.of<GoogleSignInProvider>(context,
-                                  listen: false)
-                              .user!
-                              .email,
-                          callback: (isSuccess, error) {
-                            if (isSuccess) {
-                              print("login Success..");
-                              Navigator.pushNamed(context, LoginRoutes.app);
-                              setState(() {
-                                isLoggingIn = false;
-                              });
-                            } else {
-                              print("error..");
-                              setState(() {
-                                isSuccess = false;
-                              });
-                              AmityDialog().showAlertErrorDialog(
-                                  title: "error!", message: error!);
-                            }
-                          },
-                        );
-                      } else {
-                        setState(() {
-                          isLoggingIn = false;
-                        });
-                      }
-                    });
-                  }
-                },
-                icon: Image.asset('assets/Icons/ic_login_google.png', scale: 3),
-                color: Theme.of(context).scaffoldBackgroundColor,
-                textColor: Colors.black,
-              ),
-              Spacer(),
-              SizedBox(height: 20),
-            ],
+                CustomButton(
+                  label: 'Google',
+                  radius: 12,
+                  borderColor: Colors.black,
+                  onTap: () {
+                    vm.loginWithGoogleAuth();
+                  },
+                  icon:
+                      Image.asset('assets/Icons/ic_login_google.png', scale: 3),
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  textColor: Colors.black,
+                ),
+                Spacer(),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
---
+++ lib/provider/ViewModel/authentication_viewmodel.dart
@@ -0,0 +1,68 @@
+import 'dart:developer';
+
+import 'package:amity_uikit_beta_service/amity_sle_uikit.dart';
+import 'package:amity_uikit_beta_service/components/alert_dialog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:verbose_share_world/app_navigation/app_navigation.dart';
+
+import 'package:verbose_share_world/utils/navigation_key.dart';
+
+import '../../auth/login_navigator.dart';
+import 'firebase_auth_viewmodel.dart';
+
+class AuthenTicationVM extends ChangeNotifier {
+  bool isLoading = false;
+  Future<void> loginWithEmailAndPassWord() async {}
+  Future<void> loginWithGoogleAuth() async {
+    if (!isLoading) {
+      isLoading = true;
+      notifyListeners();
+      Provider.of<GoogleSignInProvider>(navigatorKey.currentContext!,
+              listen: false)
+          .login((isSuccess, error) async {
+        if (isSuccess) {
+          log("tap signIn");
+
+          await enterTheAppWith(
+              userId: Provider.of<GoogleSignInProvider>(
+                      navigatorKey.currentContext!,
+                      listen: false)
+                  .user!
+                  .email);
+        } else {
+          isLoading = false;
+          notifyListeners();
+        }
+      });
+    }
+  }
+
+  Future<void> enterTheAppWith({required String userId}) async {
+    var context = navigatorKey.currentContext!;
+
+    if (userId != "") {
+      print("Navigating...");
+      await AmitySLEUIKit().registerDevice(
+        context: context,
+        userId: userId,
+        callback: (isSuccess, error) {
+          if (isSuccess) {
+            print("login Success..");
+            isLoading = false;
+            notifyListeners();
+            print("Navigate To app");
+            Navigator.pushNamed(navigatorKey.currentContext!, LoginRoutes.app);
+          } else {
+            print("error..");
+
+            isLoading = false;
+            notifyListeners();
+            AmityDialog()
+                .showAlertErrorDialog(title: "error!", message: error!);
+          }
+        },
+      );
+    }
+  }
+}
