9c13350eae340d4d842eeba9172b2938656c20f3
markamity <worachote@amity.co>
2022-09-16 00:09:15 +0700

    Merge code

--- lib/community_webview_screen.dart
+++ lib/community_webview_screen.dart
@@ -1,27 +1,27 @@
-import 'package:flutter/cupertino.dart';
-import 'package:webviewx/webviewx.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:webviewx/webviewx.dart';
 
-class AmityCommunityWebPage extends StatefulWidget {
-  @override
-  AmityCommunityWebPageState createState() => AmityCommunityWebPageState();
-}
+// class AmityCommunityWebPage extends StatefulWidget {
+//   @override
+//   AmityCommunityWebPageState createState() => AmityCommunityWebPageState();
+// }
 
-class AmityCommunityWebPageState extends State<AmityCommunityWebPage> {
-  late WebViewXController webviewController;
-  @override
-  void initState() {
-    super.initState();
-  }
+// class AmityCommunityWebPageState extends State<AmityCommunityWebPage> {
+//   late WebViewXController webviewController;
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
+//   @override
+//   Widget build(BuildContext context) {
+//     final mediaQuery = MediaQuery.of(context);
 
-    return WebViewX(
-        height: mediaQuery.size.height,
-        width: mediaQuery.size.width,
-        initialContent: "https://community.amity.co",
-        initialSourceType: SourceType.url,
-        onWebViewCreated: (controller) => webviewController = controller);
-  }
-}
+//     return WebViewX(
+//         height: mediaQuery.size.height,
+//         width: mediaQuery.size.width,
+//         initialContent: "https://community.amity.co",
+//         initialSourceType: SourceType.url,
+//         onWebViewCreated: (controller) => webviewController = controller);
+//   }
+// }
